dd04b0b28f88bd7a41e496c2f52ac0d93d7f59df
isabell-ah <wanzalaisabella@gmail.com>
2025-04-19 14:04:37 +0300

    add comments

--- python/week5/polymorphism.py
+++ python/week5/polymorphism.py
@@ -7,9 +7,11 @@ class Animal:
 class Dog(Animal):
     def move(self):
         print("Dog runs")
+        # bird
 class Bird(Animal):
     def move(self):
         print("Bird flies")
+        # fish
 class Fish(Animal):
     def move(self):
         print("Fish swims")
